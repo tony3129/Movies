4f108ae5ba0c3d9cad3b13d70e7398ece76ddfab
Tony Liu <tliu55@myseneca.ca>
2025-08-02 09:48:54 -0400

    set up useEffect api calls

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,31 @@ import { useEffect, useState } from 'react';
 import MovieCard from './components/MovieCard';
 
 function App() {
+  //states to hold movie information
   const [trending, setTrending] = useState([]);
   const [selectedGenre, setSelectedGenre] = useState([]);
   const [genres, setGenres] = useState(null);
   const [genreMovies, setGenreMovies] = useState([]);
+
+  //calls to populate trending and genre data on first mount
+  useEffect(() =>{
+    fetch('/api/trending')
+    .then((res) => res.json())
+    .then((data) => setTrending(data.results));
+
+    fetch('api/genres')
+    .then((res) => res.json())
+    .then((data) => setGenres(data.results));
+  }, []);
+
+  //populate genremovies based on selected genre
+  useEffect(()=>{
+    if(selectedGenre){
+      fetch(`/api/genres/${selectedGenre}`)
+      .then((res) => res.json())
+      .then((data) => setGenreMovies(data.results));
+    }
+  },[selectedGenre]);
   return (
     <></>
   );
