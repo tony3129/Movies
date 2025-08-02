f10040522eb17d0181028c5e506465447e3c5d52
Tony Liu <tliu55@myseneca.ca>
2025-08-02 09:30:56 -0400

    set up dependencies, required states

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+//set up dependenciees
 import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import MovieCard from './components/MovieCard';
 
 function App() {
+  const [trending, setTrending] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState([]);
+  const [genres, setGenres] = useState(null);
+  const [genreMovies, setGenreMovies] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
 
