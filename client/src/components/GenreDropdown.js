16300ae9f897e94a52e3c6161ad5747c1c653aa3
Tony Liu <tliu55@myseneca.ca>
2025-08-02 21:06:38 -0400

    missed return keyword for genre mapping

--- client/src/components/GenreDropdown.js
+++ client/src/components/GenreDropdown.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-function GenreDropdown({ genres, onSelect }) {
+function GenreDropdown({ genres = [], onSelect }) {
     return (
         //setSelectedGenre from App.js is passed to onSelect
         <select onChange={(e) => onSelect(e.target.value)} defaultValue="">
             <option disabled value="">Choose a genre</option>
             {/*create options for each genre type*/}
             {genres.map((genre)=>{
-                <option key={genre.id} value={genre.id}>
+                return (
+                    <option key={genre.id} value={genre.id}>
                     {genre.name}
-                </option>
+                    </option>
+                )
             })}
         </select>
     )
