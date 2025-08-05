faf6cd5a21231f8be3a766068eb3732f1e242ce3
Tony Liu <tliu55@myseneca.ca>
2025-08-05 14:43:57 -0400

    add temp styling for clickable movie cards

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 //template for movie cards
-function MovieCard({ movie }){
+function MovieCard({ movie, onClick }){
     return (
-        <div style={{ width: '150px', margin: '0.5rem' }}>
+        <div onClick={onClick} style={{ 
+            cursor: 'pointer',
+            border: '1px solid #ccc',
+            padding: '0.5rem',
+            width: '150px', 
+            margin: '0.5rem' }}>
             <img
                 //from https://developer.themoviedb.org/docs/image-basics
                 src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
