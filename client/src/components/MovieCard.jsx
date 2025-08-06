22cdd92634bd4ee0e19fe4460e2f2eef1a754758
Tony Liu <tliu55@myseneca.ca>
2025-08-06 18:37:14 -0400

    removed temp styling for daisyui and tailwind styling

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
-//template for movie cards
-function MovieCard({ movie, onClick }){
-    return (
-        <div onClick={onClick} style={{ 
-            cursor: 'pointer',
-            border: '1px solid #ccc',
-            padding: '0.5rem',
-            width: '150px', 
-            margin: '0.5rem' }}>
-            <img
-                //from https://developer.themoviedb.org/docs/image-basics
-                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                alt={movie.title}
-                style={{ width: '100%' }}
-            />
-            <h4>{movie.title}</h4>
-        </div>
-    )
+function MovieCard({ movie, onClick }) {
+  return (
+    <div
+      onClick={onClick}
+      className="card card-xs bg-base-100 shadow-md cursor-pointer rounded-lg hover:shadow-xl transition-all duration-300 ease-in-out"
+    >
+      <figure>
+        <img
+          src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+          alt={movie.title}
+        />
+      </figure>
+      <div className="card-body">
+        <h4 className="card-title text-sm">{movie.title}</h4>
+      </div>
+    </div>
+  );
 }
 
 export default MovieCard;
