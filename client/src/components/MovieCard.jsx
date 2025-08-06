e5ca66c273c4bbd85983f8603fbde5d9203a046b
Tony Liu <tliu55@myseneca.ca>
2025-08-06 18:45:58 -0400

    added comments for context

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 function MovieCard({ movie, onClick }) {
   return (
+    //based on daisyUI card styling
     <div
       onClick={onClick}
       className="card card-xs bg-base-100 shadow-md cursor-pointer rounded-lg hover:shadow-xl transition-all duration-300 ease-in-out"
     >
       <figure>
         <img
+            //from https://developer.themoviedb.org/docs/image-basics
           src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
           alt={movie.title}
         />
