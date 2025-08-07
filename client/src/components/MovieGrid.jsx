305c83d081eb59e26acc5ab27f0c92793276f058
Tony Liu <tliu55@myseneca.ca>
2025-08-06 22:05:56 -0400

    changed centering, and added max card size

--- client/src/components/MovieGrid.jsx
+++ client/src/components/MovieGrid.jsx
@@ -4,15 +4,19 @@ import MovieCard from './MovieCard';
 //component to fill out movie cards for search, trending, and genres
 function MovieGrid({ movies, onMovieClick }) {
   return (
-    <div className="flex flex-wrap gap-4 mb-8">
+    <div className="flex flex-wrap gap-4 mb-8 justify-center">
         {/*create MovieCard for each movie*/}
         {movies.map((movie) => (
-            <MovieCard
-              key={movie.id}
-              movie={movie}
-              onClick={() => onMovieClick(movie.id)}
-            />
-        ))}
+            <div 
+              key={movie.id} 
+              className="w-[220px]"
+            >
+              <MovieCard
+                movie={movie}
+                onClick={() => onMovieClick(movie.id)}
+              />
+            </div>
+          ))}
     </div>
   );
 }
