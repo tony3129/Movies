e8f4de1724a4a62d6dab832dea1608f47e2629ed
Tony Liu <tliu55@myseneca.ca>
2025-08-07 11:37:37 -0400

    added process.env.port for deployment

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
 app.use('/api', movieRoutes);
 
 //set localhost port to 5000
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
