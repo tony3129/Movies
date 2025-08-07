871354175961e3aa68271516fe81689e3a6f7fc2
Tony Liu <tliu55@myseneca.ca>
2025-08-06 22:05:07 -0400

    adjusted card height and width to stay consistent regardless of poster or movie name

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -5,17 +5,27 @@ function MovieCard({ movie, onClick }) {
     //based on daisyUI card styling
     <div
       onClick={onClick}
-      className="card card-xs bg-base-100 shadow-md cursor-pointer rounded-lg hover:shadow-xl transition-all duration-300 ease-in-out"
+      className="card card-xs bg-base-100 shadow-md cursor-pointer rounded-lg hover:shadow-1xl hover:scale-[1.02] transition-all duration-300 ease-in-out w-full flex flex-col"
     >
-      <figure>
-        <img
+      <figure className="h-[300px] w-full overflow-hidden flex-shrink-0">
+        {/*To account for empty movie posters*/}
+        {movie.poster_path ? (
+          <img
             //from https://developer.themoviedb.org/docs/image-basics
-          src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-          alt={movie.title}
-        />
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={movie.title}
+            className="w-full h-full object-cover"
+          />
+        ) : (
+          <div className="w-full h-full bg-gray-300 flex items-center justify-center text-gray-500">
+            No Image
+          </div>
+        )}
       </figure>
-      <div className="card-body">
-        <h4 className="card-title text-sm">{movie.title}</h4>
+      <div className="card-body p-2 flex-grow">
+        <h4 className="card-title text-sm truncate" title={movie.title}>
+          {movie.title}
+        </h4>
       </div>
     </div>
   );
