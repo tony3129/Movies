9e910e2795195968dd54a2fda282bde409bf5ed0
Tony Liu <tliu55@myseneca.ca>
2025-08-07 12:10:11 -0400

    to allow my deployed front-end access

--- server/index.js
+++ server/index.js
@@ -5,12 +5,24 @@ const express = require('express');
 const cors = require('cors');
 const movieRoutes = require('./routes/movies');
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://tonys-movies-app-frontend.onrender.com', // render frontend url
+];
+
 //mount 
 const app = express();
 //restrict access to only my frontend
 app.use(cors({
-    origin: 'http://localhost:3000'
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // allow non-browser tools
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  }
 }));
+
 app.use('/api', movieRoutes);
 
 //set localhost port to 5000
