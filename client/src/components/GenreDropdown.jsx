2d681cc8bd66bc6e04c031bcb833d5019fc84ee3
Tony Liu <tliu55@myseneca.ca>
2025-08-06 22:03:10 -0400

    changed dropdown to automatically load first option, changed styling to match rest of app

--- client/src/components/GenreDropdown.jsx
+++ client/src/components/GenreDropdown.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-function GenreDropdown({ genres = [], onSelect }) {
+function GenreDropdown({ genres = [], onSelect, selectedGenre }) {
     return (
         //setSelectedGenre from App.js is passed to onSelect
-        <select onChange={(e) => onSelect(e.target.value)} defaultValue="">
-            <option disabled value="">Choose a genre</option>
+        <select 
+            onChange={(e) => onSelect(e.target.value)} 
+            value={genres.length === 0 ? "" : selectedGenre}
+            className="select border border-gray-400 rounded-lg"
+        >
             {/*create options for each genre type*/}
             {genres.map((genre)=>{
                 return (
                     <option key={genre.id} value={genre.id}>
-                    {genre.name}
+                    {' '}{genre.name}
                     </option>
                 )
             })}
